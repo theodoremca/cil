f0b47b1557d26f71eb7c125eceeb0cc4f4b790c6
Theodore <theodoreimonigie@gmail.com>
2021-09-27 15:48:46 +0100

    nextur

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -17,7 +17,7 @@ class RouteServiceProvider extends ServiceProvider
      *
      * @var string
      */
-    public const HOME = '/add-blog';
+    public const HOME = '/';
 
     /**
      * The controller namespace for the application.
